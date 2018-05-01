9221fa73f6ab2b0032f264dd925c9ec9ba32ffe2
DAIRYMASTER\malam <malam@dairymaster.com>
2018-05-01 17:46:12 +0100

    Education & Experience

--- StudentPortalAPI/Models/IdentityModels.cs
+++ StudentPortalAPI/Models/IdentityModels.cs
@@ -39,5 +39,9 @@ namespace StudentPortalAPI.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<StudentPortalAPI.Models.Education> Educations { get; set; }
+
+        public System.Data.Entity.DbSet<StudentPortalAPI.Models.Experience> Experiences { get; set; }
     }
 }

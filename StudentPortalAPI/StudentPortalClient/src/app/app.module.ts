0cb3200c02ee1539243844c52a7a7e33b307b18c
Mohammad Alam <mshahnawaz9935@gmail.com>
2021-06-10 18:00:53 +0100

    HomeComponent

--- StudentPortalAPI/StudentPortalClient/src/app/app.module.ts
+++ StudentPortalAPI/StudentPortalClient/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
